d373ccb58ef2d4cef4613be590b0bac5b86c9759
Peter Suschlik <ps@neopoly.de>
2013-07-23 10:56:32 +0200

    Bootstrap with "bundler"

---
+++ lib/phpmyadmin/backup.rb
@@ -0,0 +1,7 @@
+require "phpmyadmin/backup/version"
+
+module Phpmyadmin
+  module Backup
+    # Your code goes here...
+  end
+end
---
+++ lib/phpmyadmin/backup/version.rb
@@ -0,0 +1,5 @@
+module Phpmyadmin
+  module Backup
+    VERSION = "0.0.1"
+  end
+end
